5e60916950390eded8a99d4d9fc066ba9d4f0797
Gabs <hatshex@gmail.com>
2016-10-19 20:57:08 -0500

    Update configurescript.sh

--- utils/configurescript.sh
+++ utils/configurescript.sh
@@ -3,12 +3,12 @@
 # Recibe $1 origin repo, $2 username, $3 remote repo, $4 shortcut remote repo
 # Pre-requisitos, el archivo debe tener permisos de ejecución, en caso de que no: sudo chmod x+ configurescript.sh
 # Ejemplos de ejecución: 
-# ./configurescript.sh intro-to-data-science hatshex ITAM-DS introDS
-# ./configurescript.sh programming-for-ds hatshex ITAM-DS pfds
+# ./configurescript.sh intro-to-data-science hatshex ITAM-DS introDS hatshex@gmail.com
+# ./configurescript.sh programming-for-ds hatshex ITAM-DS pfds hatshex@gmail.com
 # ********************************* By hatshex *******************************
 
-git config --global user.name "hatshex"
-git config --global user.email "hatshex@gmail.com"
+git config --global user.name $2
+git config --global user.email $5
 git config --global color.ui "auto"
 git config --global core.editor "emacs"
 git init
