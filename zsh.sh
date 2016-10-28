f44266c9d843ce91a4749a692e9b18b74e2cecd8
Gabs <hatshex@gmail.com>
2016-10-28 15:19:27 -0500

    Create zsh.sh
    Cambiando el shell a zsh, se ve mas bonito

---
+++ zsh.sh
@@ -0,0 +1,9 @@
+
+sudo apt-get -y zsh
+
+## Forzamos que el shell del usuario sea zsh
+sudo chsh -s /bin/zsh hatshex
+
+## Instalamos Oh-my-ZSH
+sudop git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
+    cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
