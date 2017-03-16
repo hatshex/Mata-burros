ae6f6a161cdbef27b4e18e75256f7e78e040ac23
Gabs <hatshex@gmail.com>
2017-03-16 11:27:30 -0600

    Update and rename tensorflow.sh to pythonjupytertensorflow.sh

--- utils/pythonjupytertensorflow.sh
+++ utils/pythonjupytertensorflow.sh
@@ -1,4 +1,5 @@
 sudo apt update && sudo apt upgrade && sudo apt install -y python-pip python-dev python3-pip
 sudo pip install --upgrade pip
+sudo pip3 install jupyter
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc1-cp35-cp35m-linux_x86_64.whl
 sudo pip3 install --upgrade $TF_BINARY_URL
