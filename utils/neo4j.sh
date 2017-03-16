22d10ec959780c1b279a86915f267c769cf6d07c
Gabs <hatshex@gmail.com>
2017-03-16 11:35:31 -0600

    Create neo4j.sh

---
+++ utils/neo4j.sh
@@ -0,0 +1,7 @@
+sudo wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb https://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt-get update
+sudo apt-get install neo4j
+sudo service neo4j status
+sudo pip install neo4jrestclient
+sudo systemctl enable neo4j
