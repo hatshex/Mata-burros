e1c2aeb0ed51aeddacf28f7d271f9b2edd66bcbe
Gabs <hatshex@gmail.com>
2017-03-16 10:55:58 -0600

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -5,5 +5,5 @@ sudo apt-get install -y zsh
 sudo chsh -s /bin/zsh hatshex
 
 ## Instalamos Oh-my-ZSH
-sudop git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
+sudo git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh && \
     cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
