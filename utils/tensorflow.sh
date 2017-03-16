62c0cb7bafc7b278247fa3b1686815cc3d7bd1ba
Gabs <hatshex@gmail.com>
2017-03-16 11:23:30 -0600

    Update tensorflow.sh

--- utils/tensorflow.sh
+++ utils/tensorflow.sh
@@ -1,3 +1,4 @@
 sudo apt update && sudo apt upgrade && sudo apt install -y python-pip python-dev python3-pip
+sudo pip install --upgrade pip
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc1-cp35-cp35m-linux_x86_64.whl
 sudo pip3 install --upgrade $TF_BINARY_URL
